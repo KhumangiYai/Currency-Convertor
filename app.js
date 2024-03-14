3e3ee7a6eaa45b880da4a138b314da7267180c91
KhumangiYai <yaimarankhuman2000@gmail.com>
2024-03-14 11:04:24 +0530

    Added JS and finalizeling HTML

--- app.js
+++ app.js
@@ -1,8 +1,60 @@
-const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json";
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
-// const dropdowns = document.querySelectorAll(".dropdown select")
+const dropdowns = document.querySelectorAll(".dropdown select")
+const btn = document.querySelector("form button")
+const fromCurr = document.querySelector(".from select")
+const toCurr = document.querySelector(".to select")
+const msg = document.querySelector(".msg")
 
 // for (code in countryList) {
-//     console.log(code);
+//     console.log(code, countryList[code]);
 // }
 
+for(let select of dropdowns) {
+    for (currCode in countryList) {
+        let newOption = document.createElement("option")
+        newOption.value = currCode;
+        newOption.innerText = currCode;
+        if (select.name==="from" && currCode==="USD") { 
+            newOption.selected = "selected"
+        } else if (select.name==="to" && currCode==="INR") { 
+            newOption.selected = "selected"
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener("change",(evt)=> {
+        UpdateFlag(evt.target);
+    })
+}
+
+const UpdateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    console.log(amtVal);
+    if (amtVal === 0) {
+        amtVal = 1; 
+        amount.value = 1;
+    }
+
+    console.log(fromCurr.value,toCurr.value);
+
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}`;
+
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    // console.log(data,rate);
+    
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
+});
